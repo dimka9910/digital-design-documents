94998c6dbcfc80fc9725e322808762c9d91da2c9
dimka <dimka99-10@mail.ru>
2021-05-13 21:08:21 +0300

    empty

--- documents-service/src/main/java/com/github/dimka9910/documents/service/ServiceConfig.java
+++ documents-service/src/main/java/com/github/dimka9910/documents/service/ServiceConfig.java
@@ -1,8 +1,6 @@
 package com.github.dimka9910.documents.service;
 
 import com.github.dimka9910.documents.jpa.JpaConfig;
-import com.github.dimka9910.documents.jdbc.JdbcConfig;
-import com.github.dimka9910.documents.jdbc.JdbcConfig;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
@@ -10,6 +8,5 @@ import org.springframework.context.annotation.Import;
 @Configuration
 @ComponentScan(basePackages = "com.github.dimka9910.documents.service")
 @Import({JpaConfig.class})
-//@Import({JdbcConfig.class})
 public class ServiceConfig {
 }
